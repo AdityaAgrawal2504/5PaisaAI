0f93f12832b2ee676c9f1f351fc47f95a0ae895b
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 14:58:43 +0530

    Add  src/main/java/com/omqs/constants/ErrorCode_OMQS1.java

---
+++ src/main/java/com/omqs/constants/ErrorCode_OMQS1.java
@@ -0,0 +1,16 @@
+package com.omqs.constants;
+
+/**
+ * Defines standardized error codes for the service.
+ */
+public enum ErrorCode_OMQS1 {
+    VALIDATION_ERROR,
+    PERSISTENCE_FAILURE,
+    DELETION_FAILURE,
+    RESOURCE_NOT_FOUND,
+    SERVICE_UNAVAILABLE,
+    INVALID_REQUEST_PAYLOAD
+}
+```
+```java
+// src/main/java/com/omqs/util/PayloadConverter_OMQS1.java
