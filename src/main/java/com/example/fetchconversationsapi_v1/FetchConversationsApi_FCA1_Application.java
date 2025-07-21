8bf10f8e9f39fc28c88e7ff4920e03a058f71f2a
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 14:57:47 +0530

    Add  src/main/java/com/example/fetchconversationsapi_v1/FetchConversationsApi_FCA1_Application.java

---
+++ src/main/java/com/example/fetchconversationsapi_v1/FetchConversationsApi_FCA1_Application.java
@@ -0,0 +1,17 @@
+package com.example.fetchconversationsapi_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchConversationsApi_FCA1_Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetchConversationsApi_FCA1_Application.class, args);
+    }
+
+}
+```
+```java
+// Logging Interface
+// File: AppLogger_FCA1.java
