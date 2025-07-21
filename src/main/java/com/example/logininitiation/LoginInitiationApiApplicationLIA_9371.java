814373d6964b469dc100a5153d510e249cca93fe
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 14:57:24 +0530

    Add  src/main/java/com/example/logininitiation/LoginInitiationApiApplicationLIA_9371.java

---
+++ src/main/java/com/example/logininitiation/LoginInitiationApiApplicationLIA_9371.java
@@ -0,0 +1,16 @@
+package com.example.logininitiation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginInitiationApiApplicationLIA_9371 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginInitiationApiApplicationLIA_9371.class, args);
+    }
+
+}
+```
+src/main/java/com/example/logininitiation/util/RequestLoggingInterceptorLIA_9371.java
+```java
