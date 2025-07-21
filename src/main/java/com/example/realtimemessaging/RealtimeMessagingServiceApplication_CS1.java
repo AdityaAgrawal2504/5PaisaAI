e6c40095cf7d23d233f6683c5e04d683710b0ba4
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 14:58:26 +0530

    Add  src/main/java/com/example/realtimemessaging/RealtimeMessagingServiceApplication_CS1.java

---
+++ src/main/java/com/example/realtimemessaging/RealtimeMessagingServiceApplication_CS1.java
@@ -0,0 +1,16 @@
+package com.example.realtimemessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeMessagingServiceApplication_CS1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeMessagingServiceApplication_CS1.class, args);
+    }
+
+}
+```
+src/main/java/com/example/realtimemessaging/constants/GrpcConstants_CS1.java
+```java
