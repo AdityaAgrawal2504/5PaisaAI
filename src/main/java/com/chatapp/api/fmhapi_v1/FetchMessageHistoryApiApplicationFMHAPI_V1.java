11cb9c4540f2188f518d468b2634422e3515e33e
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 14:58:07 +0530

    Add  src/main/java/com/chatapp/api/fmhapi_v1/FetchMessageHistoryApiApplicationFMHAPI_V1.java

---
+++ src/main/java/com/chatapp/api/fmhapi_v1/FetchMessageHistoryApiApplicationFMHAPI_V1.java
@@ -0,0 +1,13 @@
+package com.chatapp.api.fmhapi_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchMessageHistoryApiApplicationFMHAPI_V1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetchMessageHistoryApiApplicationFMHAPI_V1.class, args);
+    }
+
+}
