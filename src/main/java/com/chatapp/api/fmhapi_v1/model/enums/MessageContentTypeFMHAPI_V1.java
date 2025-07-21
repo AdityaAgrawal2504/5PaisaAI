da2e48b5fc464aabef8b8c7c48f47b06b0f9c01f
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 14:58:08 +0530

    Add  src/main/java/com/chatapp/api/fmhapi_v1/model/enums/MessageContentTypeFMHAPI_V1.java

---
+++ src/main/java/com/chatapp/api/fmhapi_v1/model/enums/MessageContentTypeFMHAPI_V1.java
@@ -0,0 +1,10 @@
+package com.chatapp.api.fmhapi_v1.model.enums;
+
+/**
+ * Enumeration for the type of message content.
+ */
+public enum MessageContentTypeFMHAPI_V1 {
+    TEXT,
+    IMAGE,
+    FILE
+}
