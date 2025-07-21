6685b5ae39e9e6e32120c7c4858c14e9351a5022
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 14:58:08 +0530

    Add  src/main/java/com/chatapp/api/fmhapi_v1/model/enums/ErrorCodeFMHAPI_V1.java

---
+++ src/main/java/com/chatapp/api/fmhapi_v1/model/enums/ErrorCodeFMHAPI_V1.java
@@ -0,0 +1,14 @@
+package com.chatapp.api.fmhapi_v1.model.enums;
+
+/**
+ * Enumeration of server-defined error codes.
+ */
+public enum ErrorCodeFMHAPI_V1 {
+    INVALID_PATH_PARAMETER,
+    INVALID_QUERY_PARAMETER,
+    INVALID_CURSOR,
+    UNAUTHENTICATED,
+    ACCESS_DENIED,
+    CONVERSATION_NOT_FOUND,
+    INTERNAL_SERVER_ERROR
+}
