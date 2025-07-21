4fd397a29dd611d1b03543ba86b4fac6096fb27f
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 14:58:09 +0530

    Add  src/main/java/com/chatapp/api/fmhapi_v1/model/enums/MessageStatusFMHAPI_V1.java

---
+++ src/main/java/com/chatapp/api/fmhapi_v1/model/enums/MessageStatusFMHAPI_V1.java
@@ -0,0 +1,11 @@
+package com.chatapp.api.fmhapi_v1.model.enums;
+
+/**
+ * Enumeration for the delivery status of a message.
+ */
+public enum MessageStatusFMHAPI_V1 {
+    SENT,
+    DELIVERED,
+    READ,
+    FAILED
+}
