f8a4ee45121c814b7078e01edc4b9f80395efd17
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 14:57:36 +0530

    Add  src/main/java/com/example/loginverification/LoginVerificationApiApplication_LVA1.java

---
+++ src/main/java/com/example/loginverification/LoginVerificationApiApplication_LVA1.java
@@ -0,0 +1,16 @@
+package com.example.loginverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginVerificationApiApplication_LVA1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginVerificationApiApplication_LVA1.class, args);
+    }
+
+}
+```
+src/main/java/com/example/loginverification/dto/LoginVerificationRequest_LVA1.java
+```java
